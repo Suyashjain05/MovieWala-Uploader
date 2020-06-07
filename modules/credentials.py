cee972e6e887cb7db75648ebb17495b8d7c11c5b
Suyashjain05 <53927129+Suyashjain05@users.noreply.github.com>
2020-06-08 00:54:30 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 Atul-Kadian
 
 class Creds(object):
-        TOKEN = "1112776828:AAE2bkQLHdYS8y1OG52PluFNRYNrD6qtf6I"
+        TOKEN = "1112776828:AAFhn_L5HC1wUlrr3SfwdNIcbk7sbzk07rQ"
         CLIENT_ID = "746228676390-t46nv1m8gu5v9jojcavs23j3mpth6o5m.apps.googleusercontent.com"
         CLIENT_SECRET = "tukmiR7_XPNZ9bSZYtUHNaip"
 
